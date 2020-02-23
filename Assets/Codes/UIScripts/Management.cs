cae8636367e2352f240304d890844335abd58bfe
Clacck <ogosimagu@gmail.com>
2020-02-23 11:30:41 +0700

    Update UI
    Update UI

---
+++ Assets/Codes/UIScripts/Management.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Management : MonoBehaviour
+{
+    public GameObject GameplayScene, InstructionsScene, EndgameScene, MenuScene;
+
+
+    //MenuScene
+    public void ClickOnPlay()
+    {
+        MenuScene.SetActive(false);
+        GameplayScene.SetActive(true);
+    }
+
+    public void ClickOnInstructions()
+    {
+        MenuScene.SetActive(false);
+        InstructionsScene.SetActive(true);
+    }
+
+    //InstructionsScene
+
+    public void ClickOnDone()
+    {
+        InstructionsScene.SetActive(false);
+        MenuScene.SetActive(true);
+    }
+
+    //EndgameScene
+
+    public void TryAgain()
+    {
+        EndgameScene.SetActive(false);
+        GameplayScene.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        EndgameScene.SetActive(false);
+        MenuScene.SetActive(true);
+    }
+}
